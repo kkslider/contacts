a6464a0339da06dca99a8ed4a69f7dc1eb472279
Benjamin Lee <benjamsandwich@gmail.com>
2013-12-09 17:32:46 -0800

    Add debugging gems

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,4 +13,20 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
     render :show
   end
+  
+  def edit
+    @employee = Employee.find(params[:id])
+    render :edit
+  end
+  
+  def update
+    @employee = Employee.find(params[:id])
+    
+    if @employee.update_attributes(params[:employee])
+      redirect_to employee_url(@employee)
+    else
+      flash.now[:error] = @employee.errors.full_messages
+      render :edit
+    end
+  end
 end
